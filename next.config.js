2c2fe11fc95ebf4f774e812f3cf4d29aaf124645
azu0021 <153065301+azu0021@users.noreply.github.com>
2025-11-25 12:56:16 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 빌드 중 오류 무시 (배포 성공률 최우선)
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // 외부 패키지의 최신 문법 오류를 해결해주는 설정 (필수)
   transpilePackages: ['undici', 'firebase', '@firebase/auth'],
 };
 
